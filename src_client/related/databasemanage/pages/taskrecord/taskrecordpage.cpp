e651ad68abb84c0983865d2f164b02d3972c68cb
YZG030455 <45348493+YZG030455@users.noreply.github.com>
2021-01-20 21:55:34 +0800

    增加任务记录子界面
    增加任务记录子界面

--- src_client/related/databasemanage/pages/taskrecord/taskrecordpage.cpp
+++ src_client/related/databasemanage/pages/taskrecord/taskrecordpage.cpp
@@ -5,6 +5,7 @@ namespace Related {
 	TaskRecordPage::TaskRecordPage(QWidget *parent)
 		: AbstractPage(parent)
 	{
+		init();
 	}
 
 	TaskRecordPage::~TaskRecordPage()
@@ -16,9 +17,41 @@ namespace Related {
 		return Page_TaskRecordPage;
 	}
 
-	void TaskRecordPage::init()
+	void TaskRecordPage::slotRespTabChanged(int page)
 	{
 
+	}
+
+	void TaskRecordPage::init()
+	{
+		m_tabWidget = new Base::RTabBar();
+		connect(m_tabWidget, SIGNAL(currentIndexChanged(int)), this, SLOT(slotRespTabChanged(int)));
+
+		m_tabWidget->setFixedHeight(60);
+		m_tabWidget->setTabAlignment(Base::RTabBar::AlignLeft);
+
+
+		Base::RTabButton * logbookButt = new Base::RTabButton(QStringLiteral("值班日志表"));
+		Base::RTabButton * trialSheetButt = new Base::RTabButton(QStringLiteral("试验记录表"));
+		Base::RTabButton * taskReportsButt = new Base::RTabButton(QStringLiteral("任务报告表"));
+
+		m_tabWidget->addTabButton(logbookButt, Tab_logbook);
+		m_tabWidget->addTabButton(trialSheetButt, Tab_trialSheet);
+		m_tabWidget->addTabButton(taskReportsButt, Tab_taskReports);
+
+		m_tabWidget->setFixedHeight(60);
+
+		m_stackedWidget = new QStackedWidget();
+
+		//m_logbookPage = new LogbookPage();
+		//m_stackedWidget->addWidget(m_logbookPage);
+
+
+		QVBoxLayout * mainLayout = new QVBoxLayout();
+		mainLayout->addWidget(m_tabWidget);
+		mainLayout->addWidget(m_stackedWidget);
+		mainLayout->setContentsMargins(0, 0, 0, 0);
+		setLayout(mainLayout);
 	}
 
 } //namespace Related 
--- src_client/related/databasemanage/pages/taskrecord/taskrecordpage.h
+++ src_client/related/databasemanage/pages/taskrecord/taskrecordpage.h
@@ -11,8 +11,12 @@
  #pragma once
 
 #include <QWidget>
+#include <QStackedWidget>
 
+#include <base\selfwidget\rtabbar.h>
 #include "../abstractpage.h"
+
+//#include "logbookpage.h"
 
 namespace Related {
 
@@ -26,8 +30,25 @@ namespace Related {
 
 		PageType getPageType() const;
 
+		enum TaskRecordIndex {
+			Tab_logbook,			/*!< 值班日志 */
+			Tab_trialSheet,			/*!< 试验记录 */
+			Tab_taskReports,		/*!< 任务报告 */
+		};
+
+
+	private slots:
+		void  slotRespTabChanged(int page);
+
 	private:
 		void init();
+
+	private:
+		Base::RTabBar * m_tabWidget;
+		QStackedWidget * m_stackedWidget;
+
+		//LogbookPage *m_logbookPage;
+
 	};
 
 } //namespace Related 
